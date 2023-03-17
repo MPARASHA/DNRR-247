2481d94c64a174aca847ce5b0affe8d0a2249563
mparasha <mparasha@ualberta.ca>
2023-03-17 00:09:57 -0600

    update

--- live.sh
+++ live.sh
@@ -4,5 +4,18 @@ set -e
 
 while true
 do
-ffmpeg -stream_loop -1 -re -i video.mp4 -stream_loop -1 -re -i *audio* -vcodec libx264 -pix_fmt yuvj420p -maxrate 20048k -preset veryfast -r 12 -framerate 30 -g 50 -c:a aac -b:a 128k -ar 44100 -strict experimental -video_track_timescale 1000 -b:v 150000k -f flv rtmp://den52.contribute.live-video.net/app/$TWITCH_STREAM_KEY
+ffmpeg -re 
+      -i video.mp4 
+      -pix_fmt yuvj420p 
+      -x264-params keyint=48:min-keyint=48:scenecut=-1 
+      -b:v 4500k 
+      -b:a 128k 
+      -ar 44100 
+      -acodec aac 
+      -vcodec libx264 
+      -preset medium 
+      -crf 28 
+      -threads 4 
+      -f flv 
+      rtmp://den52.contribute.live-video.net/app/$TWITCH_STREAM_KEY
 done
