365272145a7e178992af50675d0f03c6a9364892
mparasha <mparasha@ualberta.ca>
2023-03-17 17:46:38 -0600

    Stream Key is now a config var on Heroku

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # DNRR-247
-24 hour twitch livestream for rickroll or DN jokes (RR in a possible future update)
+24 hour twitch livestream for rickroll or DN jokes (RR in a possible future update).
+Created using FFMPEG, Docker on Heroku and Real-Time Messaging Protocol for Twitch
--- live.sh
+++ live.sh
@@ -1,2 +1,2 @@
-ffmpeg -stream_loop -1 -re -i "video.mkv" -c:v libx264 -preset veryfast -b:v 6000k -maxrate 6000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmp://iad05.contribute.live-video.net/app/live_721961566_dc7k5h2sFcx2ikucZTvtUfNC8Drbrc
+ffmpeg -stream_loop -1 -re -i "video.mkv" -c:v libx264 -preset veryfast -b:v 6000k -maxrate 6000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmp://iad05.contribute.live-video.net/app/$TWITCH_STREAM_KEY
 
