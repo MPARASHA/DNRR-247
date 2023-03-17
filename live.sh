17eac28a8a960b84344d96b31464fb3179613eab
mparasha <mparasha@ualberta.ca>
2023-03-17 01:09:19 -0600

    update

--- live.sh
+++ live.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-VBR="1500k"
-FPS="24"
-QUAL="superfast"
+VBR="2500k"                                    # Bitrate de la vidéo en sortie
+FPS="30"                                       # FPS de la vidéo en sortie
+QUAL="medium"
 TWITCH_STREAM_KEY="live_721961566_dc7k5h2sFcx2ikucZTvtUfNC8Drbrc"
 
 
-ffmpeg -hide_banner -loglevel error \
+ffmpeg \
     -stream_loop -1 -i "video.mp4" \
-    -c:v libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
-    -c:a aac \
-    -filter_complex "[0:a][1:a]amerge=inputs=2[aout]" \
-    -map "[aout]" -map 1:v:0 \
+    -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
+    -acodec libmp3lame -ar 44100 -threads 6 -b:a 712000 -bufsize 512k \
     -f flv rtmp://den52.contribute.live-video.net/app/$TWITCH_STREAM_KEY
