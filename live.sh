b101467fd6f6bf094c8260909f01eca81c258f0f
mparasha <mparasha@ualberta.ca>
2023-03-17 01:00:04 -0600

    update

--- live.sh
+++ live.sh
@@ -5,8 +5,7 @@ FPS="24"
 QUAL="superfast"
 TWITCH_STREAM_KEY="live_721961566_dc7k5h2sFcx2ikucZTvtUfNC8Drbrc"
 
-while true
-do
+
 ffmpeg \
     -stream_loop -1 -i "video.mp4" \
     -c:v libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
@@ -14,4 +13,3 @@ ffmpeg \
     -filter_complex "[0:a][1:a]amerge=inputs=2[aout]" \
     -map "[aout]" -map 1:v:0 \
     -f flv rtmp://den52.contribute.live-video.net/app/$TWITCH_STREAM_KEY
-done
