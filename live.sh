22519d30af3484c14f0cec36a2e4d8687ecfcb86
mparasha <mparasha@ualberta.ca>
2023-03-17 01:30:17 -0600

    update

--- live.sh
+++ live.sh
@@ -2,4 +2,4 @@
 
 ffmpeg -y -i video.mp4 -c:v libx264 -b:v 3000k -r 60 -vsync cfr -pass 1 -c:a copy -f null /dev/null
 ffmpeg -i video.mp4 -c:v libx264 -b:v 3000k -r 60 -pass 2 -c:a copy output.mp4
-ffmpeg -stream_loop -1 -i output.mp4 -c:v libx264 -s 852x480 -r 30 -f flv rtmp://den52.contribute.live-video.net/app/$TWITCH_STREAM_KEY
+ffmpeg -stream_loop -1 -i output.mp4 -vsync cfr -c:v libx264 -s 852x480 -r 30 -f flv rtmp://den52.contribute.live-video.net/app/$TWITCH_STREAM_KEY
