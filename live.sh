7ec3231fa203c2ae82cfb3ab110095ffa4417e37
mparasha <mparasha@ualberta.ca>
2023-03-17 01:55:37 -0600

    update

--- live.sh
+++ live.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ffmpeg -re -i "video.mkv" -c:v copy -c:a aac -ar 44100 -ac 1 -f flv rtmp://den52.contribute.live-video.net/app/$TWITCH_STREAM_KEY
+ffmpeg -re -i "video.mkv" -c:v libx264 -preset veryfast -b:v 6000k -maxrate 6000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmp://blr01.contribute.live-video.net/app/live_721961566_dc7k5h2sFcx2ikucZTvtUfNC8Drbrc
+
