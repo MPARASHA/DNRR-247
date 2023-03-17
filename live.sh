c06c373fcbeeba3f3fa382ea28f4a1738da5ddb5
mparasha <mparasha@ualberta.ca>
2023-03-17 01:23:37 -0600

    update

--- live.sh
+++ live.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ffmpeg -stream_loop -1 -re -i video.mp4 -loglevel warning -c:v libx264 -b:v 2M -strict -2 -flags +global_header -bsf:a aac_adtstoasc -bufsize 2100k -f flv rtmp://den52.contribute.live-video.net/app/$TWITCH_STREAM_KEY
+ffmpeg -stream_loop -1 -i video.mp4 -c:v libx264 -s 852x480 -r 30 -f flv rtmp://den52.contribute.live-video.net/app/$TWITCH_STREAM_KEY
