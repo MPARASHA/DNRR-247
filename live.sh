6325e2f7ac54e51a14535f4c46bd127bd23af69b
mparasha <mparasha@ualberta.ca>
2023-03-17 00:37:10 -0600

    update

--- live.sh
+++ live.sh
@@ -9,7 +9,7 @@ QUAL="medium"
 while true
 do
 ffmpeg \
-    -stream_loop -1 -i "video.mp4" -deinterlace \
+    -stream_loop -1 -i "video.mp4" \
     -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
     -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k \
     -f flv -f flv rtmp://den52.contribute.live-video.net/app/$TWITCH_STREAM_KEY
