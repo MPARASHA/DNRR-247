db0222a8964f0bac836fde7b484e84314b556ce6
mparasha <mparasha@ualberta.ca>
2023-03-17 02:10:38 -0600

    update

--- live.sh
+++ live.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+
 ffmpeg -re -i "video.mkv" -c:v libx264 -preset veryfast -b:v 6000k -maxrate 6000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmp://iad05.contribute.live-video.net/app/live_721961566_dc7k5h2sFcx2ikucZTvtUfNC8Drbrc
 
